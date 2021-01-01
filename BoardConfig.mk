ab4adb2843f30f350ee2c07ada1362abe56c899c
matteo0026 <marmotta200026@gmail.com>
2021-01-01 19:22:18 +0100

    matisse: device tree fixes and remove useless stuff
    - Remove backup from modem and persist partition since they can't be used. Vfat permits to wipe and backup/restore that partition, but once restored, it causes internet speed issues, so keep it back to emmc, which doesn't permit to wipe, backup and restore correctly the partition, but at least it doesn't give any problem.
    - Remove system.prop, since it seems we don't need it
    - Do some order in BoardConfig.mk and try to fix time even in backups
    - Use embedded.mk file to make TWRP
    
    These changes are made for 3.5.0_9-0 TWRP release.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,4 @@
-DEVICE_TREE := device/samsung/matisse
-
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := MSM8226
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8226
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+DEVICE_PATH := device/samsung/matisse
 
 # Architecture
 TARGET_ARCH := arm
@@ -16,23 +9,35 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+# Board
+TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+TARGET_BOOTLOADER_BOARD_NAME := MSM8226
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+BOARD_VENDOR := samsung
+
+# Boot image
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
-TARGET_KERNEL_CONFIG := twrp_matisse_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_SEPARATED_DT := true
-KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
-TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_HEADER_ARCH := arm
 
-# Use this flag if the board has a ext4 partition larger than 2gb
+# Filesystems
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CONFIG := twrp_matisse_defconfig
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485248
@@ -42,18 +47,18 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 12843662336
 BOARD_CACHEIMAGE_PARTITION_SIZE := 314572288
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_TREE)/system.prop
-
-# Recovery
+# Ramdisk
 LZMA_RAMDISK_TARGETS := boot,recovery
 
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 # TWRP
-TW_DEVICE_VERSION := 4
+TW_DEVICE_VERSION :=
 TW_THEME := landscape_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
--- omni_matisse.mk
+++ omni_matisse.mk
@@ -1,17 +1,16 @@
 # Release name
 PRODUCT_RELEASE_NAME := matisse
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 PRODUCT_DEVICE := matisse
 PRODUCT_NAME := omni_matisse
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:root/system/usr/share/zoneinfo/tzdata
